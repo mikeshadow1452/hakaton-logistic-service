8ddd3d120cf7b8e4820965bd07a3a779ece9c71b
mikeshadow1452 <maxicus54@gmail.com>
2024-04-07 17:56:56 +0300

    real init

---
+++ src/main/java/fun/digitallpepper/logistic_service/LogisticServiceApplication.java
@@ -0,0 +1,13 @@
+package fun.digitallpepper.logistic_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticServiceApplication.class, args);
+    }
+
+}
