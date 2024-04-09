16d2e3b0ca52dd8482890d6b18e0980c0a778f06
SpanchBOBCHIK <153946558+SpanchBOBCHIK@users.noreply.github.com>
2024-04-09 20:05:32 +0300

    3 кнопки

---
+++ src/main/client/src/components/test.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '../index.css'
+import Panel from "./ProfilePanel.jsx";
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Panel />
+
+)
