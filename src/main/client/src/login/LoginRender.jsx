acd5adbcda9403d6b7f466ecab7d4a1a117646ca
elykov <spanchbobnetu@yansex.ru>
2024-04-10 16:16:33 +0300

    .

---
+++ src/main/client/src/login/LoginRender.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '../index.css'
+import LoginPage from "./login.jsx";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <div className={"flex justify-center"}>
+        <LoginPage />
+    </div>
+
+)
